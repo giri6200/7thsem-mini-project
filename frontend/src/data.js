b0010ee8567b748dd5378274765dc01e8ff52341
giri6200 <giriprasad6200@gmail.com>
2020-10-03 13:28:02 +0530

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,25 @@
+export default {
+  products: [
+    {
+      _id: '1',
+      name: 'Furniture 1',
+      category: 'Chair',
+      image: '/images/d1.jpg',
+      price: 60,
+      brand: ' IKEA',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    {
+      _id: '2',
+      name: 'Furniture 2',
+      category: 'Sofa',
+      image: '/images/d1.jpg',
+      price: 50,
+      brand: ' Wood',
+      rating: 4.2,
+      numReviews: 5
+    },
+  ]
+}
